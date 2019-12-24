b989bcf0effe2fbf0eeba570e79f4e8fbb8ab6b5
makannan <Mahesh.Kannan@Oracle.Com>
2019-12-24 11:43:37 -0800

    Remove generated code

--- src/cache/proto/services_pb.d.ts
+++
@@ -1,8 +0,0 @@
-// package: coherence
-// file: services.proto
-
-import * as jspb from "google-protobuf";
-import * as messages_pb from "./messages_pb";
-import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
-import * as google_protobuf_wrappers_pb from "google-protobuf/google/protobuf/wrappers_pb";
-
