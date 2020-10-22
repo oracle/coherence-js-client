0adfbb3c2fd636bf865f3f5e4f306ccf8c7e74f1
Ryan Lubke <ryan.lubke@oracle.com>
2020-10-22 10:35:02 -0700

    Identifier updates for support scripts.

--- bin/docker-utils.sh
+++ bin/docker-utils.sh
@@ -11,44 +11,44 @@ declare -r ROOT="${PWD}"
 declare -r CONTAINER_NAME="coherence-js-test-container"
 declare -r IMAGE_NAME="oraclecoherence/coherence-ce:20.06.1"
 
-function coh-up() {
-    declare -r CONTAINER_ID=$(docker ps -a -q -f name="${CONTAINER_NAME}")
-    if [[ -n "${CONTAINER_ID}" ]]; then
-        docker start "${CONTAINER_ID}"
-    else
-        docker run -d -p 1408:1408 -p 5005:5005 --name "${CONTAINER_NAME}" -v \
-            "${ROOT}"/etc:/args "${IMAGE_NAME}"
-    fi
+function coh_up() {
+  declare -r CONTAINER_ID=$(docker ps -a -q -f name="${CONTAINER_NAME}")
+  if [[ -n "${CONTAINER_ID}" ]]; then
+    docker start "${CONTAINER_ID}"
+  else
+    docker run -d -p 1408:1408 -p 5005:5005 --name "${CONTAINER_NAME}" -v \
+      "${ROOT}"/etc:/args "${IMAGE_NAME}"
+  fi
 }
 
-function coh-down() {
-    declare -r CONTAINER_ID=$(docker ps -q -f name="${CONTAINER_NAME}")
-    if [[ -n "${CONTAINER_ID}" ]]; then
-        docker stop "${CONTAINER_ID}"
-    fi
+function coh_down() {
+  declare -r CONTAINER_ID=$(docker ps -q -f name="${CONTAINER_NAME}")
+  if [[ -n "${CONTAINER_ID}" ]]; then
+    docker stop "${CONTAINER_ID}"
+  fi
 }
 
-function coh-clean() {
-    coh-down
-    declare -r CONTAINER_ID=$(docker ps -a -q -f name="${CONTAINER_NAME}")
-    if [[ -n "${CONTAINER_ID}" ]]; then
-        docker rm "${CONTAINER_ID}"
-    fi
+function coh_clean() {
+  coh_down
+  declare -r CONTAINER_ID=$(docker ps -a -q -f name="${CONTAINER_NAME}")
+  if [[ -n "${CONTAINER_ID}" ]]; then
+    docker rm "${CONTAINER_ID}"
+  fi
 }
 
 while getopts "udc" OPTION; do
-    case "${OPTION}" in
-        u)
-            coh-up
-            ;;
-        d)
-            coh-down
-            ;;
-        c)
-            coh-clean
-            ;;
-        ?)
-            echo "Usage: $(basename "$0") [-u] [-d]"
-            ;;
-    esac
+  case "${OPTION}" in
+  u)
+    coh_up
+    ;;
+  d)
+    coh_down
+    ;;
+  c)
+    coh_clean
+    ;;
+  ?)
+    echo "Usage: $(basename "$0") [-u] [-d]"
+    ;;
+  esac
 done
--- bin/npm-post-install.sh
+++ bin/npm-post-install.sh
@@ -10,42 +10,41 @@ set -e
 declare -r ROOT="${PWD}"
 
 # Grabs the proto files from the Coherence project.
-function grab-proto-files() {
-    declare -r BASE_URL="https://raw.githubusercontent.com/oracle/coherence/master/prj/coherence-grpc/src/main/proto/"
-    declare -r PROTO_FILES=("messages.proto" "services.proto")
-    declare -r PROTO_DIR="${ROOT}/etc/proto"
+function grab_proto_files() {
+  declare -r BASE_URL="https://raw.githubusercontent.com/oracle/coherence/master/prj/coherence-grpc/src/main/proto/"
+  declare -r PROTO_FILES=("messages.proto" "services.proto")
+  declare -r PROTO_DIR="${ROOT}/etc/proto"
 
-    if [[ ! -d "${PROTO_DIR}" ]]; then
-        mkdir -p "${PROTO_DIR}"
-    fi
+  if [[ ! -d "${PROTO_DIR}" ]]; then
+    mkdir -p "${PROTO_DIR}"
+  fi
 
-    cd ${ROOT}
+  cd "${ROOT}"
 
-    for i in "${PROTO_FILES[@]}"; do curl -s "${BASE_URL}${i}" -o "${PROTO_DIR}/${i}"; done
+  for i in "${PROTO_FILES[@]}"; do curl -s "${BASE_URL}${i}" -o "${PROTO_DIR}/${i}"; done
 }
 
 # Generates and compiles the stubs generated from the installed proto files.
-function gen-compile-proto-files() {
-echo ${PWD}
-    declare -r PROTO_SRC_DIR="${ROOT}/etc/proto"
-    declare -r PROTO_GEN_SRC_DIR="${ROOT}/src/grpc"
-    declare -r PROTO_GEN_OUT_DIR="${ROOT}/lib/grpc"
+function gen_compile_proto_files() {
+  declare -r PROTO_SRC_DIR="${ROOT}/etc/proto"
+  declare -r PROTO_GEN_SRC_DIR="${ROOT}/src/grpc"
+  declare -r PROTO_GEN_OUT_DIR="${ROOT}/lib/grpc"
 
-    rm -rf ${PROTO_GEN_SRC_DIR} ${PROTO_GEN_OUT_DIR}
-    mkdir -p ${PROTO_GEN_SRC_DIR} ${PROTO_GEN_OUT_DIR}
+  rm -rf "${PROTO_GEN_SRC_DIR}" "${PROTO_GEN_OUT_DIR}"
+  mkdir -p "${PROTO_GEN_SRC_DIR}" "${PROTO_GEN_OUT_DIR}"
 
-    npx protoc --proto_path=${PROTO_SRC_DIR} \
-      --plugin=protoc-gen-ts=node_modules/.bin/protoc-gen-ts \
-      --plugin=protoc-gen-grpc=node_modules/.bin/grpc_tools_node_protoc_plugin \
-      --js_out=import_style=commonjs:${PROTO_GEN_OUT_DIR} \
-      --ts_out="service=grpc-node:${PROTO_GEN_SRC_DIR}" \
-      --grpc_out=${PROTO_GEN_OUT_DIR} \
-      ${PROTO_SRC_DIR}/*.proto
+  npx protoc --proto_path="${PROTO_SRC_DIR}" \
+    --plugin=protoc-gen-ts=node_modules/.bin/protoc-gen-ts \
+    --plugin=protoc-gen-grpc=node_modules/.bin/grpc_tools_node_protoc_plugin \
+    --js_out=import_style=commonjs:"${PROTO_GEN_OUT_DIR}" \
+    --ts_out="service=grpc-node:${PROTO_GEN_SRC_DIR}" \
+    --grpc_out="${PROTO_GEN_OUT_DIR}" \
+    "${PROTO_SRC_DIR}"/*.proto
 }
 
 function main() {
-    grab-proto-files
-    gen-compile-proto-files
+  grab_proto_files
+  gen_compile_proto_files
 }
 
 main
